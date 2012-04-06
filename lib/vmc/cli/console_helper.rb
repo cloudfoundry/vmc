cloudfoundry/vmc

commit cfb3086ba7f3ef7b5abce85268a873488d82fb9a

[CS] Make these rubies look like they weren't written by Java types

lib/vmc/cli/console_helper.rb
@@ -5,35 +5,35 @@ module VMC::Cli
   module ConsoleHelper
 
     def console_connection_info(appname)
-      app = client.app_info(appname)
-      fw = VMC::Cli::Framework.lookup_by_framework(app[:staging][:model])
-      if !fw.console
+      app = client.app_info appname
+      fw = VMC::Cli::Framework.lookup_by_framework app[:staging][:model]
+      unless fw.console
         err "'#{appname}' is a #{fw.name} application.  " +
           "Console access is not supported for #{fw.name} applications."
       end
-      instances_info_envelope = client.app_instances(appname)
+      instances_info_envelope = client.app_instances appname
       instances_info_envelope = {} if instances_info_envelope.is_a?(Array)
 
       instances_info = instances_info_envelope[:instances] || []
       err "No running instances for [#{appname}]" if instances_info.empty?
 
       entry = instances_info[0]
-      if !entry[:console_port]
+      unless entry[:console_port]
         begin
-          client.app_files(appname, '/app/cf-rails-console')
+          client.app_files appname, '/app/cf-rails-console'
           err "Console port not provided for [#{appname}].  Try restarting the app."
         rescue VMC::Client::TargetError, VMC::Client::NotFound
           err "Console access not supported for [#{appname}]. " +
             "Please redeploy your app to enable support."
         end
       end
-      conn_info = {'hostname' => entry[:console_ip], 'port' => entry[:console_port]}
+      {'hostname' => entry[:console_ip], 'port' => entry[:console_port]}
     end
 
     def start_local_console(port, appname)
-      auth_info = console_credentials(appname)
+      auth_info = console_credentials appname
       display "Connecting to '#{appname}' console: ", false
-      prompt = console_login(auth_info, port)
+      prompt = console_login auth_info, port
       display "OK".green
       display "\n"
       initialize_readline
@@ -41,22 +41,22 @@ module VMC::Cli
     end
 
     def console_login(auth_info, port)
-      if !auth_info["username"] || !auth_info["password"]
+      unless auth_info["username"] && auth_info["password"]
         err "Unable to verify console credentials."
       end
-      @telnet_client = telnet_client(port)
+      @telnet_client = telnet_client port
       prompt = nil
       err_msg = "Login attempt timed out."
       5.times do
         begin
-          results = @telnet_client.login("Name"=>auth_info["username"],
-            "Password"=>auth_info["password"]) {|line|
+          results = @telnet_client.login("Name" => auth_info["username"],
+            "Password" => auth_info["password"]) do |line|
             if line =~ /[$%#>] \z/n
               prompt = line
             elsif line =~ /Login failed/
               err_msg = line
             end
-          }
+          end
           break
         rescue TimeoutError
           sleep 1
@@ -64,7 +64,7 @@ module VMC::Cli
           #This may happen if we login right after app starts
           close_console
           sleep 5
-          @telnet_client = telnet_client(port)
+          @telnet_client = telnet_client port
         end
         display ".", false
       end
@@ -76,13 +76,12 @@ module VMC::Cli
     end
 
     def send_console_command(cmd)
-      results = @telnet_client.cmd(cmd)
-      results.split("\n")
+      results = @telnet_client.cmd cmd
+      results.split "\n"
     end
 
     def console_credentials(appname)
-      content = client.app_files(appname, '/app/cf-rails-console/.consoleaccess', '0')
-      YAML.load(content)
+      YAML.load client.app_files(appname, '/app/cf-rails-console/.consoleaccess', '0')
     end
 
     def close_console
@@ -91,8 +90,7 @@ module VMC::Cli
 
     def console_tab_completion_data(cmd)
       begin
-        results = @telnet_client.cmd("String"=> cmd + "\t", "Match"=>/\S*\n$/, "Timeout"=>10)
-        results.chomp.split(",")
+        @telnet_client.cmd "String" => cmd + "\t", "Match" => /\S*\n$/, "Timeout" => 10).chomp.split ","
       rescue TimeoutError
         [] #Just return empty results if timeout occurred on tab completion
       end
@@ -100,11 +98,11 @@ module VMC::Cli
 
     private
     def telnet_client(port)
-      Net::Telnet.new({"Port"=>port, "Prompt"=>/[$%#>] \z|Login failed/n, "Timeout"=>30, "FailEOF"=>true})
+      Net::Telnet.new "Port" => port, "Prompt" => /[$%#>] \z|Login failed/n, "Timeout" => 30, "FailEOF" => true
     end
 
     def readline_with_history(prompt)
-      line = Readline.readline(prompt, true)
+      line = Readline.readline prompt, true
       return '' if line.nil?
       #Don't keep blank or repeat commands in history
       if line =~ /^\s*$/ or Readline::HISTORY.to_a[-2] == line
@@ -114,16 +112,14 @@ module VMC::Cli
     end
 
     def run_console(prompt)
-      while(cmd = readline_with_history(prompt))
-        if(cmd == "exit" || cmd == "quit")
+      while cmd = readline_with_history(prompt)
+        if cmd == "exit" || cmd == "quit"
           #TimeoutError expected, as exit doesn't return anything
-          @telnet_client.cmd("String"=>cmd,"Timeout"=>1) rescue TimeoutError
+          @telnet_client.cmd("String" => cmd, "Timeout" => 1) rescue TimeoutError
           close_console
           break
         end
-        if !cmd.empty?
-          prompt = send_console_command_display_results(cmd, prompt)
-        end
+        prompt = send_console_command_display_results cmd, prompt unless cmd.empty?
       end
     end
 
@@ -132,7 +128,7 @@ module VMC::Cli
         lines = send_console_command cmd
         #Assumes the last line is a prompt
         prompt = lines.pop
-        lines.each {|line| display line if line != cmd}
+        lines.each { |line| display line unless line == cmd }
       rescue TimeoutError
         display "Timed out sending command to server.".red
       rescue EOFError
@@ -142,16 +138,14 @@ module VMC::Cli
     end
 
     def initialize_readline
-      if Readline.respond_to?("basic_word_break_characters=")
-        Readline.basic_word_break_characters= " \t\n`><=;|&{("
+      if Readline.respond_to? :basic_word_break_characters=
+        Readline.basic_word_break_characters = " \t\n`><=;|&{("
       end
       Readline.completion_append_character = nil
       #Assumes that sending a String ending with tab will return a non-empty
       #String of comma-separated completion options, terminated by a new line
       #For example, "app.\t" might result in "to_s,nil?,etc\n"
-      Readline.completion_proc = proc {|s|
-        console_tab_completion_data s
-      }
+      Readline.completion_proc = proc { |s| console_tab_completion_data s }
     end
   end
 end

lib/vmc/cli/manifest_helper.rb
@@ -11,17 +11,17 @@ module VMC::Cli::ManifestHelper
 
   MANIFEST = "manifest.yml"
 
-  YES_SET = Set.new(["y", "Y", "yes", "YES"])
+  YES_SET = Set.new ["y", "Y", "yes", "YES"]
 
   # take a block and call it once for each app to push/update.
   # with @application and @app_info set appropriately
-  def each_app(panic=true)
+  def each_app(panic = true)
     if @manifest and all_apps = @manifest["applications"]
-      where = File.expand_path(@path)
+      where = File.expand_path @path
       single = false
 
       all_apps.each do |path, info|
-        app = File.expand_path("../" + path, manifest_file)
+        app = File.expand_path "../" + path, manifest_file
         if where.start_with?(app)
           @application = app
           @app_info = info
@@ -32,9 +32,9 @@ module VMC::Cli::ManifestHelper
       end
 
       unless single
-        if where == File.expand_path("../", manifest_file)
+        if where == File.expand_path "../", manifest_file
           ordered_by_deps(all_apps).each do |path, info|
-            app = File.expand_path("../" + path, manifest_file)
+            app = File.expand_path "../" + path, manifest_file
             @application = app
             @app_info = info
             yield info["name"]
@@ -59,9 +59,9 @@ module VMC::Cli::ManifestHelper
     @app_info = nil
   end
 
-  def interact(many=false)
+  def interact(many = false)
     @manifest ||= {}
-    configure_app(many)
+    configure_app many
   end
 
   def target_manifest
@@ -71,22 +71,22 @@ module VMC::Cli::ManifestHelper
   def save_manifest(save_to = nil)
     save_to ||= target_manifest
 
-    File.open(save_to, "w") do |f|
+    File.open save_to, "w" do |f|
       f.write @manifest.to_yaml
     end
 
     say "Manifest written to #{save_to}."
   end
 
-  def configure_app(many=false)
+  def configure_app(many = false)
     name = manifest("name") ||
       set(ask("Application Name", :default => manifest("name")), "name")
 
     url_template = manifest("url") || DEFAULTS["url"]
     url_resolved = url_template.dup
-    resolve_lexically(url_resolved)
+    resolve_lexically url_resolved
 
-    url = ask("Application Deployed URL", :default => url_resolved)
+    url = ask "Application Deployed URL", :default => url_resolved
 
     url = url_template if url == url_resolved
 
@@ -100,14 +100,12 @@ module VMC::Cli::ManifestHelper
     unless manifest "framework"
       framework = detect_framework
       set framework.name, "framework", "name"
-      set(
-        { "mem" => framework.mem,
-          "description" => framework.description,
-          "exec" => framework.exec
-        },
-        "framework",
-        "info"
-      )
+      set { "mem"         => framework.mem,
+            "description" => framework.description,
+            "exec"        => framework.exec
+          },
+          "framework",
+          "info"
     end
 
     set ask(
@@ -126,11 +124,11 @@ module VMC::Cli::ManifestHelper
 
     unless manifest "services"
       user_services = client.services
-      user_services.sort! {|a, b| a[:name] <=> b[:name] }
+      user_services.sort! { |a, b| a[:name] <=> b[:name] }
 
       unless user_services.empty?
         if ask "Bind existing services to '#{name}'?", :default => false
-          bind_services(user_services)
+          bind_services user_services
         end
       end
 
@@ -165,11 +163,11 @@ module VMC::Cli::ManifestHelper
 
   # Detect the appropriate framework.
   def detect_framework(prompt_ok = true)
-    framework = VMC::Cli::Framework.detect(@application, frameworks_info)
-    framework_correct = ask("Detected a #{framework}, is this correct?", :default => true) if prompt_ok && framework
+    framework = VMC::Cli::Framework.detect @application, frameworks_info
+    framework_correct = ask "Detected a #{framework}, is this correct?", :default => true if prompt_ok && framework
     if prompt_ok && (framework.nil? || !framework_correct)
       display "#{"[WARNING]".yellow} Can't determine the Application Type." unless framework
-      framework = nil if !framework_correct
+      framework = nil unless framework_correct
       framework = VMC::Cli::Framework.lookup(
         ask(
           "Select Application Type",
@@ -185,38 +183,32 @@ module VMC::Cli::ManifestHelper
   end
 
   def bind_services(user_services, chosen = 0)
-    svcname = ask(
-      "Which one?",
+    svcname = ask "Which one?",
       :indexed => true,
-      :choices => user_services.collect { |p| p[:name] })
+      :choices => user_services.collect { |p| p[:name] }
 
     svc = user_services.find { |p| p[:name] == svcname }
 
     set svc[:vendor], "services", svcname, "type"
 
     if chosen + 1 < user_services.size && ask("Bind another?", :default => false)
-      bind_services(user_services, chosen + 1)
+      bind_services user_services, chosen + 1
     end
   end
 
   def create_services(services)
     svcs = services.collect(&:to_s).sort!
 
-    configure_service(
-      ask(
-        "What kind of service?",
-        :indexed => true,
-        :choices => svcs
+    configure_service ask("What kind of service?",
+      :indexed => true,
+      :choices => svcs
       )
-    )
 
-    if ask "Create another?", :default => false
-      create_services(services)
-    end
+    create_services services if ask "Create another?", :default => false
   end
 
   def configure_service(vendor)
-    default_name = random_service_name(vendor)
+    default_name = random_service_name vendor
     name = ask "Specify the name of the service", :default => default_name
 
     set vendor, "services", name, "type"
@@ -227,32 +219,32 @@ module VMC::Cli::ManifestHelper
       unless abspaths
         abspaths = {}
         apps.each do |p, i|
-          ep = File.expand_path("../" + p, manifest_file)
+          ep = File.expand_path "../" + p, manifest_file
           abspaths[ep] = i
         end
       end
 
       ordered = []
       apps.each do |path, info|
-        epath = File.expand_path("../" + path, manifest_file)
+        epath = File.expand_path "../" + path, manifest_file
 
         if deps = info["depends-on"]
           dep_apps = {}
           deps.each do |dep|
-            edep = File.expand_path("../" + dep, manifest_file)
+            edep = File.expand_path "../" + dep, manifest_file
 
             err "Circular dependency detected." if processed.include? edep
 
             dep_apps[dep] = abspaths[edep]
           end
 
-          processed.add(epath)
+          processed.add epath
 
           ordered += ordered_by_deps(dep_apps, abspaths, processed)
           ordered << [path, info]
         elsif not processed.include? epath
           ordered << [path, info]
-          processed.add(epath)
+          processed.add epath
         end
       end
 

lib/vmc/client.rb
@@ -36,7 +36,7 @@ class VMC::Client
   class HTTPException <  RuntimeError; end
 
   # Initialize new client to the target_uri with optional auth_token
-  def initialize(target_url=VMC::DEFAULT_TARGET, auth_token=nil)
+  def initialize(target_url = VMC::DEFAULT_TARGET, auth_token = nil)
     target_url = "http://#{target_url}" unless /^https?/ =~ target_url
     target_url = target_url.gsub(/\/+$/, '')
     @target = target_url
@@ -50,21 +50,21 @@ class VMC::Client
   # Retrieves information on the target cloud, and optionally the logged in user
   def info
     # TODO: Should merge for new version IMO, general, services, user_account
-    json_get(VMC::INFO_PATH)
+    json_get VMC::INFO_PATH
   end
 
   def raw_info
-    http_get(VMC::INFO_PATH)
+    http_get VMC::INFO_PATH
   end
 
   # Global listing of services that are available on the target system
   def services_info
     check_login_status
-    json_get(path(VMC::GLOBAL_SERVICES_PATH))
+    json_get path(VMC::GLOBAL_SERVICES_PATH)
   end
 
   def runtimes_info
-    json_get(path(VMC::GLOBAL_RUNTIMES_PATH))
+    json_get path(VMC::GLOBAL_RUNTIMES_PATH)
   end
 
   ######################################################
@@ -73,23 +73,23 @@ class VMC::Client
 
   def apps
     check_login_status
-    json_get(VMC::APPS_PATH)
+    json_get VMC::APPS_PATH
   end
 
-  def create_app(name, manifest={})
+  def create_app(name, manifest = {})
     check_login_status
     app = manifest.dup
     app[:name] = name
     app[:instances] ||= 1
-    json_post(VMC::APPS_PATH, app)
+    json_post VMC::APPS_PATH, app
   end
 
   def update_app(name, manifest)
     check_login_status
-    json_put(path(VMC::APPS_PATH, name), manifest)
+    json_put path(VMC::APPS_PATH, name), manifest
   end
 
-  def upload_app(name, zipfile, resource_manifest=nil)
+  def upload_app(name, zipfile, resource_manifest = nil)
     #FIXME, manifest should be allowed to be null, here for compatability with old cc's
     resource_manifest ||= []
     check_login_status
@@ -103,29 +103,29 @@ class VMC::Client
       upload_data[:application] = file
     end
     upload_data[:resources] = resource_manifest.to_json if resource_manifest
-    http_post(path(VMC::APPS_PATH, name, "application"), upload_data)
+    http_post path(VMC::APPS_PATH, name, "application"), upload_data
   rescue RestClient::ServerBrokeConnection
     retry
   end
 
   def delete_app(name)
     check_login_status
-    http_delete(path(VMC::APPS_PATH, name))
+    http_delete path(VMC::APPS_PATH, name)
   end
 
   def app_info(name)
     check_login_status
-    json_get(path(VMC::APPS_PATH, name))
+    json_get path(VMC::APPS_PATH, name)
   end
 
   def app_update_info(name)
     check_login_status
-    json_get(path(VMC::APPS_PATH, name, "update"))
+    json_get path(VMC::APPS_PATH, name, "update")
   end
 
   def app_stats(name)
     check_login_status
-    stats_raw = json_get(path(VMC::APPS_PATH, name, "stats"))
+    stats_raw = json_get path(VMC::APPS_PATH, name, "stats")
     stats = []
     stats_raw.each_pair do |k, entry|
       # Skip entries with no stats
@@ -139,22 +139,21 @@ class VMC::Client
 
   def app_instances(name)
     check_login_status
-    json_get(path(VMC::APPS_PATH, name, "instances"))
+    json_get path(VMC::APPS_PATH, name, "instances")
   end
 
   def app_crashes(name)
     check_login_status
-    json_get(path(VMC::APPS_PATH, name, "crashes"))
+    json_get path(VMC::APPS_PATH, name, "crashes")
   end
 
   # List the directory or download the actual file indicated by
   # the path.
-  def app_files(name, path, instance='0')
+  def app_files(name, path, instance = '0')
     check_login_status
-    path = path.gsub('//', '/')
-    url = path(VMC::APPS_PATH, name, "instances", instance, "files", path)
-    _, body, headers = http_get(url)
-    body
+    path = path.gsub '//', '/'
+    url = path VMC::APPS_PATH, name, "instances", instance, "files", path
+    http_get(url)[1]
   end
 
   ######################################################
@@ -164,7 +163,7 @@ class VMC::Client
   # listing of services that are available in the system
   def services
     check_login_status
-    json_get(VMC::SERVICES_PATH)
+    json_get VMC::SERVICES_PATH
   end
 
   def create_service(service, name)
@@ -192,32 +191,31 @@ class VMC::Client
 
     raise TargetError, "Service [#{service}] is not a valid service choice" unless service_hash
     service_hash[:name] = name
-    json_post(path(VMC::SERVICES_PATH), service_hash)
+    json_post path(VMC::SERVICES_PATH), service_hash
   end
 
   def delete_service(name)
     check_login_status
-    svcs = services || []
-    names = svcs.collect { |s| s[:name] }
+    names = (services || []).collect { |s| s[:name] }
     raise TargetError, "Service [#{name}] not a valid service" unless names.include? name
-    http_delete(path(VMC::SERVICES_PATH, name))
+    http_delete path(VMC::SERVICES_PATH, name)
   end
 
   def bind_service(service, appname)
     check_login_status
-    app = app_info(appname)
+    app = app_info appname
     services = app[:services] || []
     app[:services] = services << service
-    update_app(appname, app)
+    update_app appname, app
   end
 
   def unbind_service(service, appname)
     check_login_status
-    app = app_info(appname)
+    app = app_info appname
     services = app[:services] || []
-    services.delete(service)
+    services.delete service
     app[:services] = services
-    update_app(appname, app)
+    update_app appname, app
   end
 
   ######################################################
@@ -231,8 +229,8 @@ class VMC::Client
   # E.g. [{:sha1 => xxx, :size => xxx, :fn => filename}]
   def check_resources(resources)
     check_login_status
-    status, body, headers = json_post(VMC::RESOURCES_PATH, resources)
-    json_parse(body)
+    _, body, _ = json_post VMC::RESOURCES_PATH, resources
+    json_parse body
   end
 
   ######################################################
@@ -241,11 +239,7 @@ class VMC::Client
 
   # Checks that the target is valid
   def target_valid?
-    return false unless descr = info
-    return false unless descr[:name]
-    return false unless descr[:build]
-    return false unless descr[:version]
-    return false unless descr[:support]
+    return false unless descr = info && descr[:name] && descr[:build] && descr[:version] && descr[:support]
     true
   rescue
     false
@@ -253,13 +247,11 @@ class VMC::Client
 
   # Checks that the auth_token is valid
   def logged_in?
-    descr = info
-    if descr
-      return false unless descr[:user]
-      return false unless descr[:usage]
-      @user = descr[:user]
-      true
-    end
+    return false unless descr = info
+
+    return false unless descr[:user] && descr[:usage]
+    @user = descr[:user]
+    true
   end
 
   ######################################################
@@ -270,22 +262,18 @@ class VMC::Client
   # Auth token can be retained and used in creating
   # new clients, avoiding login.
   def login(user, password)
-    status, body, headers = json_post(path(VMC::USERS_PATH, user, "tokens"), {:password => password})
-    response_info = json_parse(body)
-    if response_info
-      @user = user
-      @auth_token = response_info[:token]
-    end
+    _, body, _ = json_post path(VMC::USERS_PATH, user, "tokens"), :password => password
+    return unless response_info = json_parse body
+    @user = user
+    @auth_token = response_info[:token]
   end
 
   # sets the password for the current logged user
   def change_password(new_password)
     check_login_status
-    user_info = json_get(path(VMC::USERS_PATH, @user))
-    if user_info
-      user_info[:password] = new_password
-      json_put(path(VMC::USERS_PATH, @user), user_info)
-    end
+    return unless user_info = json_get path(VMC::USERS_PATH, @user)
+    user_info[:password] = new_password
+    json_put(path(VMC::USERS_PATH, @user), user_info)
   end
 
   ######################################################
@@ -302,24 +290,24 @@ class VMC::Client
 
   def users
     check_login_status
-    json_get(VMC::USERS_PATH)
+    json_get VMC::USERS_PATH
   end
 
   def add_user(user_email, password)
-    json_post(VMC::USERS_PATH, { :email => user_email, :password => password })
+    json_post VMC::USERS_PATH, :email => user_email, :password => password
   end
 
   def delete_user(user_email)
     check_login_status
-    http_delete(path(VMC::USERS_PATH, user_email))
+    http_delete path(VMC::USERS_PATH, user_email)
   end
 
   ######################################################
 
   def self.path(*path)
-    path.flatten.collect { |x|
+    path.flatten.collect do |x|
       URI.encode x.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")
-    }.join("/")
+    end.join "/"
   end
 
   private
@@ -329,44 +317,43 @@ class VMC::Client
   end
 
   def json_get(url)
-    status, body, headers = http_get(url, 'application/json')
-    json_parse(body)
+    _, body, _ = http_get url, 'application/json'
+    json_parse body
   rescue JSON::ParserError
     raise BadResponse, "Can't parse response into JSON", body
   end
 
   def json_post(url, payload)
-    http_post(url, payload.to_json, 'application/json')
+    http_post url, payload.to_json, 'application/json'
   end
 
   def json_put(url, payload)
-    http_put(url, payload.to_json, 'application/json')
+    http_put url, payload.to_json, 'application/json'
   end
 
   def json_parse(str)
-    if str
-      JSON.parse(str, :symbolize_names => true)
-    end
+    return unless str
+    JSON.parse str, :symbolize_names => true
   end
 
   require 'rest_client'
 
   # HTTP helpers
 
-  def http_get(path, content_type=nil)
-    request(:get, path, content_type)
+  def http_get(path, content_type = nil)
+    request :get, path, content_type
   end
 
-  def http_post(path, body, content_type=nil)
-    request(:post, path, content_type, body)
+  def http_post(path, body, content_type = nil)
+    request :post, path, content_type, body
   end
 
-  def http_put(path, body, content_type=nil)
-    request(:put, path, content_type, body)
+  def http_put(path, body, content_type = nil)
+    request :put, path, content_type, body
   end
 
   def http_delete(path)
-    request(:delete, path)
+    request :delete, path
   end
 
   def request(method, path, content_type = nil, payload = nil, headers = {})
@@ -380,10 +367,10 @@ class VMC::Client
     end
 
     req = {
-      :method => method, :url => "#{@target}/#{path}",
+      :method => method, :url => [@target, path].join('/'),
       :payload => payload, :headers => headers, :multipart => true
     }
-    status, body, response_headers = perform_http_request(req)
+    status, body, response_headers = perform_http_request req
 
     if request_failed?(status)
       # FIXME, old cc returned 400 on not found for file access
@@ -397,11 +384,11 @@ class VMC::Client
   end
 
   def request_failed?(status)
-    VMC_HTTP_ERROR_CODES.detect{|error_code| status >= error_code}
+    VMC_HTTP_ERROR_CODES.detect { |error_code| status >= error_code }
   end
 
   def perform_http_request(req)
-    proxy_uri = URI.parse(req[:url]).find_proxy()
+    proxy_uri = URI.parse(req[:url]).find_proxy
     RestClient.proxy = proxy_uri.to_s if proxy_uri
 
     # Setup tracing if needed
@@ -410,7 +397,7 @@ class VMC::Client
     end
 
     result = nil
-    RestClient::Request.execute(req) do |response, request|
+    RestClient::Request.execute req do |response, request|
       result = [ response.code, response.body, response.headers ]
       unless trace.nil?
         puts '>>>'
@@ -418,14 +405,14 @@ class VMC::Client
         puts "REQUEST: #{req[:method]} #{req[:url]}"
         puts "RESPONSE_HEADERS:"
         response.headers.each do |key, value|
-            puts "    #{key} : #{value}"
+          puts "    #{key} : #{value}"
         end
         puts "REQUEST_BODY: #{req[:payload]}" if req[:payload]
         puts "RESPONSE: [#{response.code}]"
         begin
-            puts JSON.pretty_generate(JSON.parse(response.body))
+          puts JSON.pretty_generate(JSON.parse(response.body))
         rescue
-            puts "#{response.body}"
+          puts "#{response.body}"
         end
         puts '<<<'
       end
@@ -450,7 +437,7 @@ class VMC::Client
   def parse_error_message(status, body)
     parsed_body = json_parse(body.to_s)
     if parsed_body && parsed_body[:code] && parsed_body[:description]
-      desc = parsed_body[:description].gsub("\"","'")
+      desc = parsed_body[:description].gsub '"', "'"
       "Error #{parsed_body[:code]}: #{desc}"
     else
       "Error (HTTP #{status}): #{body}"

lib/vmc.rb
@@ -1,4 +1,7 @@
 module VMC
+  autoload :Client, "vmc/client"
+  autoload :Micro,  "vmc/micro"
+
   def self.windows?
     !!(RUBY_PLATFORM =~ /mingw|mswin32|cygwin/)
   end

commit ca4fd64de82d1ea003f86318839a1c250a54097d

[BUGFIX] Fix some bugs introduced in cfb3086

lib/vmc/cli/console_helper.rb
@@ -90,7 +90,7 @@ module VMC::Cli
 
     def console_tab_completion_data(cmd)
       begin
-        @telnet_client.cmd "String" => cmd + "\t", "Match" => /\S*\n$/, "Timeout" => 10).chomp.split ","
+        @telnet_client.cmd("String" => cmd + "\t", "Match" => /\S*\n$/, "Timeout" => 10).chomp.split ","
       rescue TimeoutError
         [] #Just return empty results if timeout occurred on tab completion
       end

lib/vmc/cli/manifest_helper.rb
@@ -32,7 +32,7 @@ module VMC::Cli::ManifestHelper
       end
 
       unless single
-        if where == File.expand_path "../", manifest_file
+        if where == File.expand_path("../", manifest_file)
           ordered_by_deps(all_apps).each do |path, info|
             app = File.expand_path "../" + path, manifest_file
             @application = app
@@ -100,12 +100,14 @@ module VMC::Cli::ManifestHelper
     unless manifest "framework"
       framework = detect_framework
       set framework.name, "framework", "name"
-      set { "mem"         => framework.mem,
-            "description" => framework.description,
-            "exec"        => framework.exec
-          },
-          "framework",
-          "info"
+      set({
+        "mem"         => framework.mem,
+        "description" => framework.description,
+        "exec"        => framework.exec
+        },
+        "framework",
+        "info"
+        )
     end
 
     set ask(

lib/vmc/client.rb
@@ -239,7 +239,8 @@ class VMC::Client
 
   # Checks that the target is valid
   def target_valid?
-    return false unless descr = info && descr[:name] && descr[:build] && descr[:version] && descr[:support]
+    return false unless descr = info
+    return false unless descr[:name] && descr[:build] && descr[:version] && descr[:support]
     true
   rescue
     false
@@ -263,7 +264,7 @@ class VMC::Client
   # new clients, avoiding login.
   def login(user, password)
     _, body, _ = json_post path(VMC::USERS_PATH, user, "tokens"), :password => password
-    return unless response_info = json_parse body
+    return unless response_info = json_parse(body)
     @user = user
     @auth_token = response_info[:token]
   end
@@ -271,7 +272,7 @@ class VMC::Client
   # sets the password for the current logged user
   def change_password(new_password)
     check_login_status
-    return unless user_info = json_get path(VMC::USERS_PATH, @user)
+    return unless user_info = json_get(path(VMC::USERS_PATH, @user))
     user_info[:password] = new_password
     json_put(path(VMC::USERS_PATH, @user), user_info)
   end

spec/unit/console_helper_spec.rb
@@ -198,7 +198,7 @@ describe 'VMC::Cli::ConsoleHelper' do
   it 'should properly initialize Readline for tab completion' do
     @client.should_receive(:app_files).with("foo", '/app/cf-rails-console/.consoleaccess', '0').and_return(IO.read(spec_asset('console_access.txt')))
     @telnet_client.should_receive(:login).with({"Name"=>"cfuser", "Password"=>"testpw"}).and_yield("irb():001:0> ")
-    Readline.should_receive(:respond_to?).with("basic_word_break_characters=").and_return(true)
+    Readline.should_receive(:respond_to?).with(:basic_word_break_characters=).and_return(true)
     Readline.should_receive(:basic_word_break_characters=).with(" \t\n`><=;|&{(")
     Readline.should_receive(:completion_append_character=).with(nil)
     Readline.should_receive(:completion_proc=)