999ca43618bfb0098cc58db01c49b70b7b04a9c1
Alex Suraci <suraci.alex@gmail.com>
2012-11-12 01:49:22 -0800

    improve v2 vmc push url mapping flow
    * handles route being taken, and asks for another
    * asks for url after app creation
    
    Change-Id: I8af4a51f90d8579e05c4408beb54b9394d3ab1a9

--- lib/vmc/cli/app.rb
+++ lib/vmc/cli/app.rb
@@ -965,9 +965,9 @@ module VMC
 
       app.command = input[:command] if framework.name == "standalone"
 
-      url = input[:url, name]
-
-      app.urls = [url] if url && !v2?
+      if !v2? && url = input[:url, name]
+        app.urls = [url]
+      end
 
       default_memory = detector.suggested_memory(framework) || 64
       app.memory = megabytes(input[:memory, human_mb(default_memory)])
@@ -978,7 +978,27 @@ module VMC
         app.create!
       end
 
-      invoke :map, :app => app, :url => url if url && v2?
+      if v2?
+        line unless quiet?
+
+        url = input[:url, name]
+
+        mapped_url = false
+        until !url || mapped_url
+          begin
+            invoke :map, :app => app, :url => url
+            mapped_url = true
+          rescue CFoundry::APIError => e
+            raise unless e.error_code == 210003
+
+            line c(e.description, :bad)
+            line
+
+            input.forget(:url)
+            url = input[:url, name]
+          end
+        end
+      end
 
       bindings = []
 
